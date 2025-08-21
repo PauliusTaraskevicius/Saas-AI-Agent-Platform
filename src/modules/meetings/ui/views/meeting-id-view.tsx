2406dfc3b6a69d4033af2c08b9b5b355ae93e4fd
PauliusTaraskevicius <fixas1029@gmail.com>
2025-08-21 10:51:48 +0300

    20: meetings page

---
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useConfirm } from "../../hooks/use-confirm";
+import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { useState } from "react";
+
+interface MeetingIdViewProps {
+  meetingId: string;
+}
+
+export function MeetingIdView({ meetingId }: MeetingIdViewProps) {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+
+  const [updateMeetingDialogOpen, setUpdateMeetingDialogOpen] = useState(false);
+
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+
+  const removeMeeting = useMutation(
+    trpc.meetings.remove.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.meetings.getMany.queryOptions({})
+        );
+        router.push("/meetings");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const [RemoveConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    `The following action will this meeting`
+  );
+
+  async function handleRemoveMeeting() {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    await removeMeeting.mutateAsync({ id: meetingId });
+  }
+
+  return (
+    <>
+      <RemoveConfirmation />
+      <UpdateMeetingDialog
+        open={updateMeetingDialogOpen}
+        onOpenChange={setUpdateMeetingDialogOpen}
+        initialValues={data}
+      />
+      <div className="flex-1 py-1 px-4 md:px-8 flex flex-col gap-y-4">
+        <MeetingIdViewHeader
+          meetingId={meetingId}
+          meetingName={data.name}
+          onEdit={() => setUpdateMeetingDialogOpen(true)}
+          onRemove={handleRemoveMeeting}
+        />
+        {JSON.stringify(data, null, 2)}
+      </div>
+    </>
+  );
+}
+
+export function MeetingIdViewLoading() {
+  return (
+    <LoadingState
+      title="Loading Meeting"
+      description="This may take a few seconds..."
+    />
+  );
+}
+export function MeetingIdViewError() {
+  return (
+    <ErrorState
+      title="Error loading individual meeting"
+      description="Something went wrong while fetching the agents. Please try again later."
+    />
+  );
+}
